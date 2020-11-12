30061cd4d201aece744c3e3fb911dfcdf2449e39
Samantha-Pierce <spapierce@gmail.com>
2020-11-12 13:15:40 -0800

    insert method completed

--- src/main/java/Problem3/InsertInBST.java
+++ src/main/java/Problem3/InsertInBST.java
@@ -5,6 +5,21 @@ import Problem1.TreeNode;
 public class InsertInBST {
     public static void insert(TreeNode<Integer> root, int valToInsert) {
         // homework
-        
+        TreeNode<Integer> nodeToInsert = new TreeNode<>(valToInsert);
+        // base case
+        if (root == null) {
+            return;
+        }
+        if (valToInsert <= root.val) {
+            insert(root.left, valToInsert);
+            if (root.left == null) {
+                root.left = nodeToInsert;
+            }
+        } else {
+            insert(root.right, valToInsert);
+            if (root.right == null) {
+                root.right = nodeToInsert;
+            }
+        }
     }
 }
