f6714c13a99b122ca66afeee9189152f4d9960bf
Samantha-Pierce <spapierce@gmail.com>
2020-11-14 08:30:15 -0800

    implemented in-order and level-order

--- src/main/java/Problem1/LevelOrderTraverse.java
+++ src/main/java/Problem1/LevelOrderTraverse.java
@@ -1,12 +1,31 @@
 package Problem1;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class LevelOrderTraverse {
     public static List<List<Integer>> levelOrder(TreeNode<Integer> root) {
         // homework
         List<List<Integer>> result = new ArrayList<>();
-        return result;  // place holder
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            List<Integer> oneResult = new ArrayList<>();
+            TreeNode<Integer> node = q.remove();
+            oneResult.add(node.val);
+            if (node.left != null) {
+                q.add(node.left);
+            }
+            if (node.right != null) {
+                q.add(node.right);
+            }
+            result.add(oneResult);
+        }
+        return result;
     }
 }
